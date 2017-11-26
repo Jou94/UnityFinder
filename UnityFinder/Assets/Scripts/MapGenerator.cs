2f7f701b16dda03f7e9cc37929ba76a5a471533d
Jordi Jounou <j.jounou94@gmail.com>
2017-11-26 06:25:22 +0100

    Map refined + First cursor test

--- UnityFinder/Assets/Scripts/MapGenerator.cs
+++ UnityFinder/Assets/Scripts/MapGenerator.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour {
 
 	public Transform tilePrefab;
+	//public Transform black_background;
 	public Vector2 mapSize;
+
+	[Range(0,1)]
+	public float outlinePercent;
 
 	void Start() {
 		GenerateMap();
@@ -13,10 +17,24 @@ public class MapGenerator : MonoBehaviour {
 
 	public void GenerateMap() {
 
+		//Vector3 black_background_position = new Vector3 (0,-0.1f,0);
+		//black_background = Instantiate(black_background, black_background_position, Quaternion.Euler(Vector3.right*90)) as Transform;
+		//black_background.localScale = new Vector3((mapSize.x+1), (mapSize.y+1), 1);
+
+		string holderName = "Generated Map";
+		if (transform.FindChild(holderName)) {
+			Destroy(transform.FindChild(holderName).gameObject);
+		}
+
+		Transform mapHolder = new GameObject (holderName).transform;
+		mapHolder.parent = transform;
+
 		for (int x = 0; x < mapSize.x; x++) {
 			for (int y = 0; y < mapSize.y; y++) {
 				Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
 				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right*90)) as Transform;
+				newTile.localScale = Vector3.one * (1-outlinePercent);
+				newTile.parent = mapHolder;
 			}
 		}
 
